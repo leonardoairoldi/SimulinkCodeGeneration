1a460bc73987b85c8c2b5b5a4f6b393380b845cc
Leonardo Airoldi <leonardo.airoldi@live.com>
2024-05-15 14:47:45 +0200

    [PPT]

--- Simulink/Scripts/ImportStructs.m
+++ Simulink/Scripts/ImportStructs.m
@@ -7,6 +7,8 @@ includes = proj.RootFolder + '/Keil/Custom_Code/C_typedef/' + hArray;
 mainStruct = proj.RootFolder + '/Keil/Custom_Code/C_typedef/' + hStruct;
 
 Simulink.importExternalCTypes(mainStruct, 'IncludeDirs', includes, 'OutputDir', 'Simulink/Generated_Defines', 'Overwrite', 'on', 'Verbose', 'on');
+% Può anche definire delle define con 'Defines' con cui discriminare quale
+% codice importare (Importante per la CAN LIBRARY)
 
 GlobalVarGenerator('GLOBAL_t', 'SMKACADEMY');
 
